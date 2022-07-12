2577a48e484fa332ed6139523d3e35a14d1e4dd9
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2022-07-12 16:55:41 +0800

    extend `Ownable` from openzeppelin

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,23 +1,12 @@
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract Greeter {
+contract Greeter is Ownable {
     string private _greeting = "Hello, World!";
-    address private _owner;
-    constructor() public {
-        _owner = msg.sender;
-    }
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Ownable: caller is not the owner");
-        _;
-    }
     function greet() external view returns(string memory) {
         return _greeting;
     }
     function setGreeting(string calldata greeting) external onlyOwner{
         _greeting = greeting;
-    }
-    function owner() public view returns(address) {
-        return _owner;
     }
 }
