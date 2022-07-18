fc2e8399793565b42df94b28f7e3caada172e783
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2022-07-18 17:42:43 +0800

    format with prettier+solidity

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 contract Fundraiser {
     string public name;
     string public url;
+
     constructor(string memory _name, string memory _url) {
-      name = _name;
-      url = _url;
+        name = _name;
+        url = _url;
     }
 }
--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -4,10 +4,12 @@ import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 contract Greeter is Ownable {
     string private _greeting = "Hello, World!";
-    function greet() external view returns(string memory) {
+
+    function greet() external view returns (string memory) {
         return _greeting;
     }
-    function setGreeting(string calldata greeting) external onlyOwner{
+
+    function setGreeting(string calldata greeting) external onlyOwner {
         _greeting = greeting;
     }
 }
