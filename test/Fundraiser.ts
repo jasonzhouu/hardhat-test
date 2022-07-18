700142396c9caa37d1e4faa6368ad5b801c43b9d
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2022-07-18 16:40:14 +0800

    test: create fund raiser before each test

--- test/Fundraiser.ts
+++ test/Fundraiser.ts
@@ -1,9 +1,13 @@
 import { ethers } from "hardhat";
-import { assert, expect } from "chai";
+import { ethers as ethers_ } from "ethers";
+import { assert } from "chai";
 
 describe("FundRaiser", () => {
+  let fundraiser: ethers_.ContractFactory;
+  beforeEach(async () => {
+    fundraiser = await ethers.getContractFactory("Fundraiser");
+  });
   it("get contract", async () => {
-    const FundRaiser = await ethers.getContractFactory("Fundraiser");
-    assert(FundRaiser, "contract was not found");
+    assert(fundraiser, "contract was not found");
   });
 });
