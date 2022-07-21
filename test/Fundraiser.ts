921f6b3e7289bfd59f0cdec777f484772ec58e39
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2022-07-21 15:15:33 +0800

    test with loadFixture

--- test/Fundraiser.ts
+++ test/Fundraiser.ts
@@ -1,17 +1,18 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers } from "hardhat";
 import { ethers as ethers_ } from "ethers";
 import { assert, expect } from "chai";
 
 describe("FundRaiser", async () => {
-  let fundraiser: ethers_.Contract;
-  const accounts = await ethers.getSigners();
-  const name = "Beneficiary Name";
-  const url = "beneficiaryname.org";
-  const imageURL = "https://placekitten.com/600/350";
-  const description = "Beneficiary description";
-  const beneficiary = accounts[1].address;
-  const custodian = accounts[0].address;
-  beforeEach(async () => {
+  async function deployFixture() {
+    let fundraiser: ethers_.Contract;
+    const accounts = await ethers.getSigners();
+    const name = "Beneficiary Name";
+    const url = "beneficiaryname.org";
+    const imageURL = "https://placekitten.com/600/350";
+    const description = "Beneficiary description";
+    const beneficiary = accounts[1].address;
+    const custodian = accounts[0].address;
     const Fundraiser = await ethers.getContractFactory("Fundraiser");
     fundraiser = await Fundraiser.deploy(
       name,
@@ -21,8 +22,19 @@ describe("FundRaiser", async () => {
       beneficiary,
       custodian
     );
-  });
+    return {
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+      custodian,
+      fundraiser,
+    };
+  }
+
   it("get contract", async () => {
+    const { name, fundraiser } = await loadFixture(deployFixture);
     assert(fundraiser, "contract was not found");
     const actual = await fundraiser.name();
     expect(actual).to.equal(name);
